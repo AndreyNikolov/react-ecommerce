a219c114708e80162ecd83edffa9fee912ce02ca
Andrey Nikolov <andrey.nikolov@resolvesys.com>
2020-01-30 23:59:10 +0200

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/Homepage";
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
