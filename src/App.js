843ea72aaf7c5667f9c9e9feb6886d6a4314af5b
Andrey Nikolov <andrey.nikolov@resolvesys.com>
2020-03-20 16:32:46 +0200

    add navbar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shoppage/Shoppage";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
