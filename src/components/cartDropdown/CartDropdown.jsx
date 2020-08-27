8901e2eb307025c396f033702d020e7d67e487c0
Andrey Nikolov <andrey.nikolov@smule.com>
2020-08-27 23:33:12 +0300

    introduce reselect

--- src/components/cartDropdown/CartDropdown.jsx
+++ src/components/cartDropdown/CartDropdown.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../customButton/CustomButton';
+import CartItem from '../cartItem/CartItem';
+import { selectCartItems } from '../../redux/cart/cartSelectors';
 
 import './CartDropwdown.scss';
 
-const CartDropwdown = () => (
+const CartDropwdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropwdown;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropwdown);
